e9c7d4b2dcf3dce528d97977930497783657eb71
Marama77 <marama@gmx.ch>
2026-03-09 20:29:25 +0100

    Copied the index.js from teacher, created view.js with import & export, changes the buttons-color & form, include bundle.js

---
+++ src/index.js
@@ -0,0 +1,53 @@
+import hh from "hyperscript-helpers";
+import { h, diff, patch } from "virtual-dom";
+import createElement from "virtual-dom/create-element";
+
+//imports function view from view.js
+import { view } from './view.js';
+
+// Messages which can be used to update the model
+const MSGS = {
+    UPDATE_MODEL: "UPDATE_MODEL",
+    UPDATE_RANDOM_NUMBER: "UPDATE_RANDOM_NUMBER",
+    // ... ℹ️ additional messages
+  };
+
+  // Update function which takes a message and a model and returns a new/updated model
+function update(msg, model) {
+    switch (msg) {
+      case MSGS.UPDATE_MODEL:
+        return { ...model, currentTime: new Date().toLocaleTimeString() };
+  
+      case MSGS.UPDATE_RANDOM_NUMBER:
+        return { ...model, randomNumber: Math.random() };
+      default:
+        return model;
+    }
+  }
+
+  // ⚠️ Impure code below (not avoidable but controllable)
+function app(initModel, update, view, node) {
+    let model = initModel;
+    let currentView = view(dispatch, model);
+    let rootNode = createElement(currentView);
+    node.appendChild(rootNode);
+    function dispatch(msg) {
+      model = update(msg, model);
+      const updatedView = view(dispatch, model);
+      const patches = diff(currentView, updatedView);
+      rootNode = patch(rootNode, patches);
+      currentView = updatedView;
+    }
+  }
+
+  // The initial model when the app starts
+const initModel = {
+    currentTime: new Date().toLocaleTimeString(),
+    randomNumber: 1,
+  };
+  
+  // The root node of the app (the div with id="app" in index.html)
+  const rootNode = document.getElementById("app");
+  
+  // Start the app
+  app(initModel, update, (dispatch, model) => view(dispatch, model, MSGS), rootNode);
---
+++ src/view.js
@@ -0,0 +1,20 @@
+import hh from "hyperscript-helpers";
+import { h } from "virtual-dom";
+
+const { div, button, p, h1 } = hh(h);
+
+const btnStyle = "bg-indigo-500 hover:bg-fuchsia-500 text-white font-bold py-2 px-4 rounded-full";
+  
+  // View function which represents the UI as HTML-tag functions
+  export function view(dispatch, model, MSGS) {
+    return div({ className: "flex flex-col gap-4 items-center" }, [
+      h1({ className: "text-2xl" }, `My Title`),
+      button({ className: btnStyle, onclick: () => dispatch(MSGS.UPDATE_MODEL) }, "Update Model"),
+      p({ className: "text-2xl" }, `Time: ${model.currentTime}`),
+      button({ className: btnStyle, onclick: () => dispatch(MSGS.UPDATE_RANDOM_NUMBER) }, "Update Random Number"),
+      p({ className: "text-2xl" }, `Random Number: ${model.randomNumber}`),
+      // ... ℹ️ additional elements
+    ]);
+  }
+
+ 
